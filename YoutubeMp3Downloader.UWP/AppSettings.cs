e4a80f0266b1af6cee9ccb629a71d908e43ff97b
Juan G. Carmona <juan@jgcarmona.com>
2022-06-12 11:18:46 +0200

    UWP pipeline

---
+++ YoutubeMp3Downloader.UWP/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+
+namespace YoutubeMp3Downloader.UWP
+{
+    public class AppSettings : IAppSettings
+    {
+        public AppSettings()
+        {
+
+            var resourceLoader = ResourceLoader.GetForCurrentView("appsettings");
+            AppVersion = resourceLoader.GetString(nameof(AppVersion));
+            AppCenterSecret = resourceLoader.GetString(nameof(AppCenterSecret));
+        }
+        public string AppVersion { get; }
+        public string AppCenterSecret { get; }
+    }
+}
---
+++ YoutubeMp3Downloader.UWP/IAppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeMp3Downloader.UWP
+{
+    public interface IAppSettings
+    {
+        string AppVersion { get; }
+        string AppCenterSecret { get; }
+    }
+}
