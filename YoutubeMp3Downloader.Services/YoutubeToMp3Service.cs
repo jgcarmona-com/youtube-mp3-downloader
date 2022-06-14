b138299ba440a82de7c04f3a8b575b69fd4ae321
Juan G. Carmona <juan@jgcarmona.com>
2022-06-14 11:30:11 +0200

    Replace MediaToolkit with Xabe.ffmpeg

--- YoutubeMp3Downloader.Services/YoutubeToMp3Service.cs
+++ YoutubeMp3Downloader.Services/YoutubeToMp3Service.cs
@@ -1,50 +1,72 @@
-﻿using MediaToolkit;
-using MediaToolkit.Model;
-using VideoLibrary;
+﻿using VideoLibrary;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
 
 namespace YoutubeMp3Downloader.Services;
 
 public class YoutubeToMp3Service : IYoutubeToMp3Service
 {
 
-    public void DownloadMp3ByUrl(string url)
+    public async void DownloadMp3ByUrl(string url)
     {
         try
         {
-            Console.Write($"Downloading {url} :: ");
+
             var path = "C:/YoutubeMp3Downloader";
             if (!Directory.Exists(path))
             {
+                InstallFfmpeg();
                 Directory.CreateDirectory(path);
             }
+            Console.Write($"Downloading");
             var folder = path + "/";
             var youtube = YouTube.Default;
             Console.Write(".");
             var vid = youtube.GetVideo(url);
             Console.Write(".");
+            var inputFileName = folder + vid.FullName;
             File.WriteAllBytes(folder + vid.FullName, vid.GetBytes());
-            Console.Write(".");
+            Console.WriteLine(".");
 
-            var inputFile = new MediaFile { Filename = folder + vid.FullName };
-            var mp3Filename = folder + vid.FullName.Replace(".mp4", "") + ".mp3";
-            var outputFile = new MediaFile { Filename = mp3Filename };
-
-            using (var engine = new Engine())
+            var mp3FileName = folder + vid.FullName.Replace(".mp4", "") + ".mp3";
+            if (File.Exists(mp3FileName))
             {
-                Console.Write(".");
-                engine.GetMetadata(inputFile);
-                Console.Write(".");
-                engine.Convert(inputFile, outputFile);
-                Console.Write(".");
+                Console.WriteLine("The file already exists.");
+                File.Delete(inputFileName);
+                return;
             }
-            File.Delete(folder + vid.FullName);
-            Console.Write(".");
-            Console.WriteLine($"\r\nDownloaded {mp3Filename}");
+            Task.Run(async () =>
+            {
+                var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(inputFileName, mp3FileName);
+                Console.WriteLine("Converting to mp3...");
+                conversion.OnProgress += async (sender, args) =>
+                {
+                    await Console.Out.WriteAsync($"\r[{args.Duration}/{args.TotalLength}][{args.Percent}%]");
+                };
+                await conversion.SetAudioBitrate(320000).SetOutputFormat(Format.mp3).Start();
+                Console.WriteLine();
+                Console.WriteLine("Saving Audio file");
+
+            }).Wait();
+            Console.WriteLine("Deleting Video file");
+            File.Delete(inputFileName);
+            Console.WriteLine($"[DONE!] => {mp3FileName}");
+            Console.WriteLine();
         }
         catch (Exception ex)
         {
+            Console.WriteLine();
             Console.WriteLine(ex.Message);
+            if (ex.Message.ToLower().Contains("ffmpeg"))
+            {
+                Console.WriteLine("It looks like FFMPEG is not installed in your system... We are installing it for you");
+                InstallFfmpeg();
+            }
         }
+    }
 
+    private void InstallFfmpeg()
+    {
+        Task.Run(async () => { await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official); }).Wait();
     }
 }
